fd7b1d341bfb1c076edbda047d6755e1c5d11eba
Italo Bessa <89542693+itallobessa@users.noreply.github.com>
2025-05-13 11:39:11 -0300

    Ultimas aulas
    treinos, informações da aula

---
+++ Treino/Aulas/aula.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+int main(){
+
+    int idade = 30;
+    int quantidade = 1;
+    float altura = 1.70;
+    double peso = 75.0;
+    char letra =  'A';
+    char nome[5] = "Italo";
+
+    idade = 30;
+     
+}
